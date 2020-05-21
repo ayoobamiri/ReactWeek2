cc8de6606c1e5003635bfceced71aafef3e953f0
Mohammad Amiri <mohmd.a.a@gmail.com>
2020-05-21 04:39:59 -0700

    initial upload

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,19 @@
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community; 
+    }
+// default methods
+    // roar() {
+    //     console.log(`The ${this.color} dragon lets out a tremendous roar!`);
+    // }
+}
+
+class Bootcamp {
+    constructor(name, level, students){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+}
